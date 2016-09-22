1412a2a104a0205c235bef909596bfc49ca9689c
Tom <tom.hewlett.taylor@googlemail.com>
2016-09-22 11:55:46 +0100

    Add sublistmax function.

--- Search.hs
+++ Search.hs
@@ -10,3 +10,13 @@ binarysearch x y = let l = length x
                            else case binarysearch (drop (index + 1) x) y of
                                      Nothing -> Nothing
                                      Just z -> Just (z + index + 1)
+
+sublistmax :: (Num a, Ord a) => [a] -> a
+sublistmax (x:xs) = fst (foldl (\(m, c) n ->
+    if n >=0
+    then (if c + n > m
+          then (c + n, c + n)
+          else (m, c + n))
+    else (if n > m
+          then (n, 0)
+          else (m, 0))) (x, x) xs)
